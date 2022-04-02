7e32ddfaab4ac5598bb32c2addebd369078c6d3e
Halim <61380938+book1001@users.noreply.github.com>
2022-04-02 18:53:31 -0400

    Add files via upload

---
+++ script_eyeview.js
@@ -0,0 +1,10 @@
+
+
+function eyeview() {
+  var x = document.getElementById("eyeviewShow");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
